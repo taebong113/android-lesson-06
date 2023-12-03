d39fb1c85832ccbbaff7ebb5e0211251f36494d2
taebong113 <105273042+taebong113@users.noreply.github.com>
2023-12-03 17:22:14 +0900

    Update UserAuthEndpoint.java

--- src/main/java/kr/easw/lesson06/controller/UserAuthEndpoint.java
+++ src/main/java/kr/easw/lesson06/controller/UserAuthEndpoint.java
@@ -6,10 +6,7 @@ import kr.easw.lesson06.model.dto.UserDataEntity;
 import kr.easw.lesson06.service.UserDataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,25 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserAuthEndpoint {
     private final UserDataService userDataService;
 
-
-    // JWT 인증을 위해 사용되는 엔드포인트입니다.
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody UserDataEntity entity) {
         try {
             // 로그인을 시도합니다.
-            return ResponseEntity.ok(userDataService.createTokenWith(entity));
+            String token = String.valueOf(userDataService.createTokenWith(entity));
+            return ResponseEntity.ok(token); // 성공 시 JWT 토큰 반환
         } catch (Exception ex) {
-            // 만약 로그인에 실패했다면, 400 Bad Request를 반환합니다.
+            // 로그인 실패 시, 예외 메시지와 함께 Bad Request 반환
             return ResponseEntity.badRequest().body(new ExceptionalResultDto(ex.getMessage()));
         }
     }
 
-
     @PostMapping("/register")
-    public void register(@ModelAttribute UserDataEntity entity) {
-        // 유저 회원가입을 구현하십시오.
-        // 해당 메서드를 작성하기 위해서는, UserDataService와 admin_dashboard.html을 참고하십시오.
-        // 해당 메서드는 register.html에서 사용됩니다.
-        throw new IllegalStateException("Not implemented yet");
+    public ResponseEntity<Object> register(@RequestBody UserDataEntity entity) {
+        try {
+            // 유저 회원가입 로직을 구현합니다.
+            userDataService.registerUser(entity);
+            return ResponseEntity.ok("User registered successfully");
+        } catch (Exception ex) {
+            // 회원가입 실패 시, 예외 메시지와 함께 Bad Request 반환
+            return ResponseEntity.badRequest().body(new ExceptionalResultDto(ex.getMessage()));
+        }
     }
 }
